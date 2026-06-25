9f4b69ce2a9b210f4efd79f849e89d0342f82804
IT Intern 101 <adrian.200018@hotmail.com>
2026-06-25 21:19:28 +0800

    Set up GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/mvp-prototype",
+  assetPrefix: "/mvp-prototype/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
